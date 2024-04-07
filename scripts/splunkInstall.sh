f94015365d6a49745bc9764d3aed64b031dd1611
Sheikh Munawar <sheikhmunawar@gmail.com>
2024-04-07 10:15:05 +0500

    start creating splunkInstall.sh script on the base image

--- scripts/splunkInstall.sh
+++ scripts/splunkInstall.sh
@@ -1,5 +1,9 @@
 #!/bin/bash
 
+
+
+/opt/splunkforwarder/bin/splunk start --accept-license --answer-yes --no-prompt --seed-passwd Pakistan
+
 delete
 rm -rf /opt/splunk/etc/instance.cfg
 sed -i "s/^serverName.*/serverName = $(hostname)/" /opt/splunk/etc/system/local/server.conf
